085e231f0fd759bdf94d80d7488baecb00167592
hiepnvph28430 <mrhiep165@gmail.com>
2023-05-15 21:47:25 +0700

    push

--- src/app/datas/mockData.ts
+++ src/app/datas/mockData.ts
@@ -2,9 +2,9 @@ import { IProduct } from "src/common/product";
 
 export const product: IProduct[] = [
     { id: "1", name: "San Pham 1", price: 9898, desc: "ABC", imageUrl: "https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" },
-    { id: "2", name: "San Pham 2", price: 988, desc: "ABC", imageUrl: "https://picsum.photos/125/50" },
-    { id: "3", name: "San Pham 3", price: 2898, desc: "ABC", imageUrl: "https://picsum.photos/125/50" },
-    { id: "4", name: "San Pham 4", price: 5898, desc: "ABC", imageUrl: "https://picsum.photos/125/50" },
-    { id: "5", name: "San Pham 5", price: 923498, desc: "ABC", imageUrl: "https://picsum.photos/125/50" },
-    { id: "6", name: "San Pham 6", price: 1298, desc: "ABCD", imageUrl: "https://picsum.photos/125/50" }
+    { id: "2", name: "San Pham 2", price: 988, desc: "ABC", imageUrl: "https://salt.tikicdn.com/cache/w1200/ts/product/71/e4/14/54c5c9a28d1516c658910499d3f1f237.jpg" },
+    { id: "3", name: "San Pham 3", price: 2898, desc: "ABC", imageUrl: "https://salt.tikicdn.com/cache/w1200/ts/product/6c/65/29/5aa4d63164c8d02ba7b9bfe8ace56a1b.jpg" },
+    { id: "4", name: "San Pham 4", price: 5898, desc: "ABC", imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsP9J7IV51ZWLY1Ox5bdbEGpQcd3jbOmCWLA&usqp=CAU" },
+    { id: "5", name: "San Pham 5", price: 923498, desc: "ABC", imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqSuS7g_mMgFfe7GUWf4pxW7Y4S4VJjo1HoA&usqp=CAU" },
+    { id: "6", name: "San Pham 6", price: 1298, desc: "ABCD", imageUrl: "https://lzd-img-global.slatic.net/g/p/405d0ec354a361baed1541936f683444.jpg_720x720q80.jpg_.webp" }
 ]
